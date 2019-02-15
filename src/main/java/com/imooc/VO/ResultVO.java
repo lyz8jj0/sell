8cfa28270a7a15d5924161d83b2aceef1d7e8e06
lyz8jj0 <844216765@qq.com>
2019-02-15 18:39:39 +0800

    买家商品信息接口完成,等待部署nginx来测试微信页面

---
+++ src/main/java/com/imooc/VO/ResultVO.java
@@ -0,0 +1,25 @@
+package com.imooc.VO;
+
+import lombok.Data;
+
+/**
+ * http请求返回的最外层对象
+ */
+@Data
+public class ResultVO<T> {
+
+    /**
+     * 错误码
+     */
+    private Integer code;
+
+    /**
+     * 提示信息
+     */
+    private String msg;
+
+    /**
+     * 具体信息
+     */
+    private T data;
+}
